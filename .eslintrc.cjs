d3737983d91157d238212f792177c5b94f409604
Abhishek047 <abhi.shukla0047@gmail.com>
2024-01-17 13:37:22 +0530

    Husky added but not working lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {
+    // we can add and turn off rules here
+    "react/react-in-jsx-scope": "off",
+    "react/button-has-type": "off",
+  },
+};
